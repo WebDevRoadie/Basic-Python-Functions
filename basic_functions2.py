2eae1cc8e8566b14779cbe48bd3a64619765fbc3
James Lawrence <73411441+WebDevRoadie@users.noreply.github.com>
2021-01-25 14:25:14 -0800

    Add files via upload

---
+++ basic_functions2.py
@@ -0,0 +1,43 @@
+#1
+def countdown(num):
+    nums_list = []
+    for val in range(num,-1,-1):
+        nums_list.append(val)
+    return nums_list
+
+print(countdown(12))
+    
+#2
+def print_and_return(nums_list):
+    print(nums_list[0])
+    return nums_list[1]
+
+print(print_and_return([10,12]))
+
+#3
+def first_plus_length(nums_list):
+    return nums_list[0] + len(nums_list)
+
+print(first_plus_length([13,2,3,4,5]))
+
+#4
+def values_greater_than_second(orig_list):
+    new_list = []
+    second_val = orig_list[1]
+    for idx in range(len(orig_list)):
+        if orig_list[idx] > second_val:
+            new_list.append(orig_list[idx])
+    
+    print(len(new_list))
+    return new_list
+
+print(values_greater_than_second([5,2,3,2,1,4]))
+
+#5
+def length_and_value(size, value):
+    new_list = []
+    for num_times in range(size):
+        new_list.append(value)
+    return new_list
+
+print(length_and_value(4,7))
